76179919e900f56c5f0f14e663cb004f57f9c5c7
jchawkins725 <jchawkins725@gmail.com>
2019-03-18 15:29:59 -0400

    javascript additions

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var slider = document.getElementById("slider");
 var output = document.getElementById("output");
+var price = document.getElementById("price");
+var tip = document.getElementById("tip");
+var final = document.getElementById("final");
 
 slider.oninput = function() {
   output.innerHTML = this.value + "%";
+  tip.value = slider.value;
 }
 output.innerHTML = slider.value + "%";
 
@@ -11,28 +15,38 @@ document.getElementById("terrible").addEventListener("click",
 function () {
     slider.value = 5;
     output.innerHTML = slider.value + "%";
+    tip.value = slider.value;
   }
 );
 document.getElementById("bad").addEventListener("click",
 function () {
     slider.value = 10;
     output.innerHTML = slider.value + "%";
+    tip.value = slider.value;
   }
 );
 document.getElementById("satisfactory").addEventListener("click",
 function () {
     slider.value = 15;
     output.innerHTML = slider.value + "%";
+    tip.value = slider.value;
   }
 );document.getElementById("good").addEventListener("click",
 function () {
     slider.value = 20;
     output.innerHTML = slider.value + "%";
+    tip.value = slider.value;
   }
 );document.getElementById("great").addEventListener("click",
 function () {
     slider.value = 25;
     output.innerHTML = slider.value + "%";
+    tip.value = slider.value;
   }
 );
 
+document.getElementById("calculate").addEventListener("click",
+function () {
+  final.innerHTML = "$" + (tip.value * price.value / 100).toFixed(2);
+}
+);
